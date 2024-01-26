c40bc7c26a826a0b6c97267f2984019a2cf7e28b
g3nsy <g3nsy@proton.me>
2024-01-26 23:43:42 +0100

    update

--- script.py
+++ script.py
@@ -14,7 +14,9 @@ with open(data_path, mode="r", encoding="UTF-8") as f:
     os.system(f'sudo pacman --needed -S {" ".join(data["proglist"])}')
 
     if shutil.which("yay") is None:
-        os.system("git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si")
+        os.system(
+            "git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si"
+        )
 
     os.system(f'yay --needed -S {" ".join(data["aurproglist"])}')
 
@@ -22,6 +24,17 @@ with open(data_path, mode="r", encoding="UTF-8") as f:
         dest = os.path.join(home, dest)
         os.makedirs(dest, exist_ok=True)
         os.system(f"cp -rfv {os.path.join(here, cfile)} {dest}")
+
+    os.makedirs(os.path.join(home, ".local/share/fonts"), exist_ok=True)
+    os.system(
+        f'unzip {os.path.join(here, "Hack.zip")} -d  {os.path.join(home, ".local/share/fonts")}'
+    )
+    os.system("fc-cache -fv")
+
+    os.system("git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm")
+    os.system("sudo pacman -R vim")
+    os.system("sudo ln -sfv /usr/bin/nvim /usr/bin/vim")
+    os.system(f"pip install {' '.join(data['pyglib'])}")
 
 
 for sh_script in os.listdir(path=sh):
