c381d79a209072cce0f0ef291895f483ef5b0f99
g3nsy <g3nsy@proton.me>
2024-01-27 01:35:08 +0100

    update

--- script.py
+++ script.py
@@ -13,42 +13,42 @@ with open(data_path, mode="r", encoding="UTF-8") as f:
 
     os.system(f'sudo pacman --needed -S {" ".join(data["proglist"])}')
 
-    if shutil.which("yay") is None:
-        os.system(
-            "git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si"
-        )
-
-    os.system(f'yay --needed -S {" ".join(data["aurproglist"])}')
-
-    for cfile, dest in data["configs"].items():
-        dest = os.path.join(home, dest)
-        os.makedirs(dest, exist_ok=True)
-        os.system(f"cp -rfv {os.path.join(here, cfile)} {dest}")
-
-    os.makedirs(os.path.join(home, ".local/share/fonts"), exist_ok=True)
-    os.system(
-        f'unzip -o {os.path.join(here, "Hack.zip")} -d  {os.path.join(home, ".local/share/fonts")}'
-    )
-    os.system("fc-cache -fv")
-
-    os.system("git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm")
-    os.system("sudo pacman -R vim")
-    os.system("sudo ln -sfv /usr/bin/nvim /usr/bin/vim")
-    os.system(f"pip install {' '.join(data['pyglib'])}")
-
-    theme_path = os.path.join(here, "theme")
-    os.system(
-        f"sudo tar -v -xf {os.path.join(theme_path, 'cursor.tar.gz')} -C /usr/share/icons/"
-    )
-    os.system(
-        f"sudo tar -v -xf {os.path.join(theme_path, 'icons.tar.xz')} -C /usr/share/icons/"
-    )
-    os.system(
-        f"sudo tar -v -xf {os.path.join(theme_path, 'theme.tar.xz')} -C /usr/share/themes/"
-    )
-
-
-for sh_script in os.listdir(path=sh):
-    os.system(
-        f"cp -v {os.path.join(sh, sh_script)} {os.path.join(home, '.local/bin/')}"
-    )
+#     if shutil.which("yay") is None:
+#         os.system(
+#             "git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si"
+#         )
+#
+#     os.system(f'yay --needed -S {" ".join(data["aurproglist"])}')
+#
+#     for cfile, dest in data["configs"].items():
+#         dest = os.path.join(home, dest)
+#         os.makedirs(dest, exist_ok=True)
+#         os.system(f"cp -rfv {os.path.join(here, cfile)} {dest}")
+#
+#     os.makedirs(os.path.join(home, ".local/share/fonts"), exist_ok=True)
+#     os.system(
+#         f'unzip -o {os.path.join(here, "Hack.zip")} -d  {os.path.join(home, ".local/share/fonts")}'
+#     )
+#     os.system("fc-cache -fv")
+#
+#     os.system("git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm")
+#     os.system("sudo pacman -R vim")
+#     os.system("sudo ln -sfv /usr/bin/nvim /usr/bin/vim")
+#     os.system(f"pip install {' '.join(data['pyglib'])}")
+#
+#     theme_path = os.path.join(here, "theme")
+#     os.system(
+#         f"sudo tar -v -xf {os.path.join(theme_path, 'cursor.tar.gz')} -C /usr/share/icons/"
+#     )
+#     os.system(
+#         f"sudo tar -v -xf {os.path.join(theme_path, 'icons.tar.xz')} -C /usr/share/icons/"
+#     )
+#     os.system(
+#         f"sudo tar -v -xf {os.path.join(theme_path, 'theme.tar.xz')} -C /usr/share/themes/"
+#     )
+#
+#
+# for sh_script in os.listdir(path=sh):
+#     os.system(
+#         f"cp -v {os.path.join(sh, sh_script)} {os.path.join(home, '.local/bin/')}"
+#     )
