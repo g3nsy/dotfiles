eff1a0fbf6de5f27cf959752d6a30919fd3d96d8
Simone Gentili (g3nsy) <g3nsy@proton.me>
2024-02-05 16:13:02 +0100

    update

--- scripts/py/i3smart_workspaces.py
+++ scripts/py/i3smart_workspaces.py
@@ -16,33 +16,39 @@ def is_HDMI_connected() -> bool:
 
 def get_xrandr_screens() -> list:
     comm = sp.run("xrandr", shell=True, stdout=-1, stderr=-1)
-    return [x.split()[0] for x in comm.stdout.decode().split("\n")[1:]
-            if x and not x.startswith(' ')]
+    return [
+        x.split()[0]
+        for x in comm.stdout.decode().split("\n")[1:]
+        if x and not x.startswith(" ")
+    ]
 
 
 def get_workspaces_by(i3: i3ipc.connection.Connection, screen: str) -> list:
-    return [x for x in i3.get_workspaces() if x.ipc_data['output'] == screen]
+    return [x for x in i3.get_workspaces() if x.ipc_data["output"] == screen]
 
 
-def move_workspaces_to(i3: i3ipc.connection.Connection,
-                       workspaces: list,
-                       direction: str) -> None:
+def move_workspaces_to(
+    i3: i3ipc.connection.Connection, workspaces: list, direction: str
+) -> None:
     for w in workspaces:
         i3.command(f"workspace {w.num}")
         i3.command(f"move workspace to output {direction}")
 
 
-def is_workspace_empty(i3: i3ipc.connection.Connection,
-                       workspace: i3ipc.replies.WorkspaceReply) -> bool:
-    workspace = i3.get_tree().find_by_id(workspace.ipc_data['id'])
+def is_workspace_empty(
+    i3: i3ipc.connection.Connection, workspace: i3ipc.replies.WorkspaceReply
+) -> bool:
+    workspace = i3.get_tree().find_by_id(workspace.ipc_data["id"])
     return not workspace or not workspace.workspace().leaves()
 
 
-def handler_function(i3: i3ipc.connection.Connection,
-                     i3thread: threading.Thread,
-                     sec: int = 5,
-                     direction_HDMI: str = "left",
-                     direction_PRIMARY: str = "right") -> None:
+def handler_function(
+    i3: i3ipc.connection.Connection,
+    i3thread: threading.Thread,
+    sec: int = 5,
+    direction_HDMI: str = "left",
+    direction_PRIMARY: str = "right",
+) -> None:
     HDMI = get_xrandr_screens()[1]
     left_move = not is_HDMI_connected()
     right_move = left_move
@@ -59,10 +65,11 @@ def handler_function(i3: i3ipc.connection.Connection,
             left_move = True
         if HDMI_connected and HDMI_workspaces and not right_move:
             xrandr_auto.main()
-            move_workspaces_to(i3,
-                               [x for x in HDMI_workspaces
-                                if not is_workspace_empty(i3, x)],
-                               direction_PRIMARY)
+            move_workspaces_to(
+                i3,
+                [x for x in HDMI_workspaces if not is_workspace_empty(i3, x)],
+                direction_PRIMARY,
+            )
             time.sleep(3)
             i3.command("restart")
             right_move = True
